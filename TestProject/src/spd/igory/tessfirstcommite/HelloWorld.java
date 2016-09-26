4ed669bd0b8d87536088fd88f155e2efa9b7feb8
igory <igor.yurko.mail@gmail.com>
2016-09-26 21:41:49 +0300

    safsdf

--- TestProject/src/spd/igory/tessfirstcommite/HelloWorld.java
+++ TestProject/src/spd/igory/tessfirstcommite/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         String str = "";
-
+        System.out.println(str);
+        if (str == null) {
+            return;
+        }
         System.out.println("Hello World!!!");
 
     }
